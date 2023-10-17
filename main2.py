fbfa9d67a9d8c69042995c7764d5b8f629418e55
Bushkov Evgeniy <144938081+ZheBush@users.noreply.github.com>
2023-10-17 22:58:53 +0300

    Delete main2.py

--- main2.py
+++
@@ -1,9 +0,0 @@
-import wikipedia
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-app = FastAPI()
-
-@app.get('/{query}')
-def qp(query: str):
-    return 'Query: ' + query + '. Path: ' + wikipedia.page(query).url
