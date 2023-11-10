99371f2727c22a115c880bd0be8e4b1125da3e6f
Bushkov Evgeniy <buschkow2005@gmail.com>
2023-11-10 23:00:14 +0300

    3 блока без текста

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Tasks from "./components/Tasks";
 import './index.css'
 
@@ -9,28 +8,6 @@ function App() {
       <Tasks></Tasks>
       <Tasks></Tasks>
       <Tasks></Tasks>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 318157c9a4fc706fcb468d6672575c0af20b0a1f
     </div>
   );
 }
--- lab4/src/index.js
+++ lab4/src/index.js
@@ -2,20 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-<<<<<<< HEAD
 );
-=======
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 318157c9a4fc706fcb468d6672575c0af20b0a1f
