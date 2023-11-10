c5fcb9f5e92d9185bf5ae014fa08b617848f667d
Bushkov Evgeniy <144938081+ZheBush@users.noreply.github.com>
2023-11-10 20:51:57 +0300

    Add files via upload

---
+++ lab4/src/App.js
@@ -0,0 +1,13 @@
+import './components/Tasks'
+import Tasks from './components/Tasks';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Tasks></Tasks>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab4/src/components/Tasks.js
@@ -0,0 +1,14 @@
+import './Tasks.css';
+
+function Tasks() {
+
+    const task = 'Изучение React';
+
+    return (
+        <div className = 'task'>
+            {task}
+        </div>
+    );
+}
+
+export default Tasks;
